cc9ec14768ebb2537bfdc164c11a7a1197c7f9d5
paarth <paarthaggarwal418@gmail.com>
2024-04-03 15:30:44 +0530

    View Binding

---
+++ ViewBinding&CheckBox/app/src/main/java/com/example/viewbinding/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.viewbinding
+
+import android.content.res.ColorStateList
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
+import com.example.viewbinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            if(binding.checkBox.isChecked){
+
+            }else{
+                binding.checkBox.buttonTintList = ColorStateList.valueOf(Color.RED)
+                Toast.makeText(this, "Please accept T&C", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+
+}
